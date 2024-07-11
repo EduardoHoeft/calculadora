72ad1e18c9f75af12eac79043c8356e99bcc3be4
EduardoHoeft <eduardofelipehoeft@gmail.com>
2024-07-11 08:36:59 -0300

    correção

--- soma2num.js
+++ soma2num.js
@@ -1,7 +1,7 @@
 var n1 = Number(window.prompt('Digite um número: '))
 
-        var n2 = Number(window.prompt('Digite outro número: '))
+var n2 = Number(window.prompt('Digite outro número: '))
 
-        var soma = n1 + n2
+var soma = n1 + n2
 
-        window.alert(`A soma entre ${n1} e ${n2} é igual a ${soma}`) //concatenação
+window.alert(`${n1} + ${n2} = ${soma}`) //concatenação
